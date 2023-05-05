39c54ad0de8ba2f996d133c58621c61ca255d4ba
username <pedropessoamonteiro@gmail.com>
2023-05-05 15:07:57 +0100

    Gráfico V(I) e interseções

--- projeto/batata.m
+++ projeto/batata.m
@@ -9,32 +9,21 @@ n_iteracoes=3;
 
 I=Vs/Rs;
 x = linspace(0, I, 10000);
-fonte_y= Vs - Rs * x;
-plot(x, fonte_y, 'r', 'LineWidth', 2); hold on;
+f = @(x) Vs - Rs .* x;
+plot(x, f(x), 'r', 'LineWidth', 2); hold on;
 
 
 %% Carga
 
-carga_y = RL_CC * x;
-plot(x, carga_y, 'b', 'LineWidth', 2); grid on;
+c = @(x) RL_CC .* x;
+plot(x, c(x), 'b', 'LineWidth', 2); grid on;
 
 %% Ponto de operaçao
 
+zero_x = fzero(@(x) f(x) - c(x), 2);
+zero_y = f(zero_x);
 
-
-y= carga_y - fonte_y;
-zer = find(y==0);
-if isempty(zer)
-            disp('FDS');
-            y=round(y, 1);
-            zer=find(y==0);
-            zer=zer(1);
-        end
-
-P=[zer*I/(10000), carga_y(zer)]
-
-plot(P(1), P(2), 'o', 'MarkerFaceColor','k');
-
+plot(zero_x, zero_y, 'o', 'MarkerFaceColor','k');
 xlabel('Corrente (A)'); ylabel('Tensao (V)');
 %legend('Fonte', 'Carga', 'Ponto de operacao', 'Location','best');
 
@@ -42,42 +31,31 @@ xlabel('Corrente (A)'); ylabel('Tensao (V)');
 %% Tensao na fonte e carga
 
 ponto_i = [0 0];
+zer_x = 0;
+zer_y= 0;
+
 for k=0:n_iteracoes
 
     if mod(k, 2) == 0
-        b=ponto_i(2) - Z0*ponto_i(1)
-        y1= Z0*x + b; 
+        b = zer_y - Z0 * zer_x;
+        y1 = @(x) Z0.*x + b;
         
-        aux=fonte_y-y1;
-        zer1=find(aux==0);
-        if isempty(zer1)
-            disp('FDS');
-            aux=round(aux, 1);
-            zer1=find(aux==0);
-            zer1=zer1(1);
-        end
-        
-        ponto_i=[zer1*I/10000, y1(zer1)]
-        plot(x, y1, 'k--'); hold on;
-        plot(ponto_i(1), ponto_i(2), 'o', 'MarkerFaceColor','y');
+        zer_x = fzero(@(x) f(x) - y1(x), 1);
+        zer_y = y1(zer_x);
+
+        plot(x, y1(x), 'k--'); hold on;
+        plot(zer_x, zer_y, 'o', 'MarkerFaceColor','y');
         
     
     else
-        b=ponto_i(2) + Z0*ponto_i(1)
-        y2=-Z0*x + b;
-        plot(x, y2, 'k--'); hold on;
-        aux= y2 - carga_y;
-        zer1=find(aux==0)
+        b = zer_y + Z0 * zer_x;
+        y2 = @(x) -Z0.*x + b;
         
-        if isempty(zer1)
-            disp('FDS');
-            aux=round(aux, 1);
-            zer1=find(aux==0);
-            zer1=zer1(1);
-        end
-
-        ponto_i=[zer1*I/10000, y2(zer1)]
-        plot(ponto_i(1), ponto_i(2), 'o', 'MarkerFaceColor','y');
+        zer_x = fzero(@(x) c(x) - y2(x), 1);
+        zer_y = y2(zer_x);
+        
+        plot(x, y2(x), 'k--'); hold on;
+        plot(zer_x, zer_y, 'o', 'MarkerFaceColor','y');
     end
 end
 
